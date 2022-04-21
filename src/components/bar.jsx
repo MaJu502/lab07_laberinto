247d01ed66f031b7cc9634ccb474016cd31a7cc2
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-04-21 15:29:17 -0600

    bar done

---
+++ src/components/bar.jsx
@@ -0,0 +1,20 @@
+/*  Universidad del Valle de Guatemala
+    Marco Jurado 20308
+
+    header_bar.jsx 
+    header que aparece en todo el juego
+*/
+import React from 'react'
+import '../styles/bar.css'
+import carritologo from '../imagepool/sprite_car_celebration.gif'
+
+const bar = () => {    
+    return (
+        <header>
+            <img src={carritologo} alt="logo pac-man"/>
+            <h1> Esto no es temple run, juega </h1>
+        </header>
+    )
+}
+
+export default bar
