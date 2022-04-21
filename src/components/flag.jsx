cc366fe845c78abc504bdb1e3104329b9d813d57
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-04-21 14:59:46 -0600

    flag done

--- src/components/flag.jsx
+++ src/components/flag.jsx
@@ -4,3 +4,10 @@
     flag.jsx
     componente de la meta del laberinto
 */
+import React from "react";
+const flag = () => {
+    /* bandera de llegada */
+    return( <div className={"wall meta"}> <div className="bandera"></div> </div>)
+}
+
+export default flag
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const generadorLaberinto = (a,b) => {
+    const callResponse = await fetch(
+        "https://maze.juanelcaballo.club/?type=json&w=$%7Bw%7D&h=$%7Bh%7D"
+        .then((callResponse) => callResponse.json())
+        .then((info) => info);
+    return callResponse.json();
+}
+
 const App = () => {
     return <h1>Esto no es temple run, adelante</h1>
 }
+
 
 ReactDOM.render(<App/>, document.getElementById('root'))
