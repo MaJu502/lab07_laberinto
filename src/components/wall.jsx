72e91d2ce92d9a85fde56ebec49d0b728eea7296
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-04-24 19:01:06 -0600

    wall jsx done

--- src/components/wall.jsx
+++ src/components/wall.jsx
@@ -5,3 +5,35 @@
     recibe del api generador de laberintos y 
     traduce a react
 */
+import React from "react";
+import car from "./car.jsx";
+import flag from "./flag.jsx";
+
+/* generar */
+const boundaries = ({ type, setMap, map, setWin }) => {
+
+    const stringType = translate(type)
+
+    if (stringType == 'flag') return <flag/>
+    if (stringType == 'car') return (
+        <car 
+            winned={setWin}
+            setMap={setMap}
+            map={map}
+        />
+    )
+
+    return <div className={'wall ' + stringType}></div> 
+}
+
+/* traducir */
+const translate = (maze_comp) => {
+    if (maze_comp === '+') return 'corner'
+    if (maze_comp === 'p') return 'car'
+    if (maze_comp === 'g') return 'flag'
+    if (maze_comp === ' ') return 'empty'
+    if (maze_comp === '|') return 'vertical'
+    if (maze_comp === '-') return 'horizontal'  
+}
+
+export default Walls
